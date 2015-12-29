193e691d7e5ea4128da221e9af566873b9b9d3a1
Next <nextacademy@email.com>
2015-12-29 02:18:42 -0800

    ajax-ed

--- app/controllers/static.rb
+++ app/controllers/static.rb
@@ -1,19 +1,21 @@
 #controller file
 get '/' do
-	@urls = Url.all
+	@urls = Url.last(5)
   	erb :"static/index"
 end
 
 post '/urls' do
 	# create a new Url
+
 	url = Url.new(long_url: params[:long_url])
 	if url.save
-		redirect '/'
+		# redirect '/'
+		return "<tr><td><a href='#{url.short_url}''>http://localhost:9393/#{url.short_url}</a></td><td>#{url.click_count}</td></tr>"
 	else
 		#Think if the error exists at url.rb?, then u can put in here
 		@errors = url.errors.full_messages
 		@urls = Url.all
-	  	erb :"static/index"
+	  	url.errors.to_json
 	end
 
 	# erb :"models/url"# or redirect to '/'
---
+++ public/js/application.js
@@ -0,0 +1,36 @@
+$(document).ready(function(){
+	$.modal($('#modal1'), 
+		{ overlayClose: true }
+	);
+
+	$(document).on('click', '.close',function(){
+		$(this).parent('#modal1').hide();
+		$(this).parent('#modal2').hide();
+	});
+
+	$('.inst_next_btn').click(function(){
+		$('#modal1').hide();
+		$('#modal2').show();
+	});
+
+	$(".form-inline").submit(function(e) {
+	    $('.score downvoted').show()
+	    $('.score unvoted').hide()
+	    $.ajax({
+	           method: "POST",
+	           url: '/urls',
+	           data: $(".form-inline").serialize(), // serializes the form's elements.
+	           success: function(data)
+	           {
+
+	           	$('#url_table tr:last').after(data)
+
+	           }
+	    });
+	    
+    	e.preventDefault(); // avoid to execute the actual submit of the form.
+	});
+});
+
+
+
